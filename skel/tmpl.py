7029682f909a300370b5781cc3faa1ce5d955553
Yuchen Ying <yegle@uga.edu>
2013-02-25 12:02:07 -0500

    Mod: add vim modline to python template file

--- skel/tmpl.py
+++ skel/tmpl.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# vim: set fileencoding=utf-8 :
+# vim: set fileencoding=utf-8 ts=4 sw=4 tw=72 :
 
 from __future__ import (unicode_literals, absolute_import,
                         division, print_function)
