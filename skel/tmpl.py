1ea67d428d0931556b2271e7f36f4110bb6e5a84
Yuchen Ying <cnyegle@gmail.com>
2014-07-30 10:05:06 -0700

    Mod: re-format Python template file

--- skel/tmpl.py
+++ skel/tmpl.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # vim: set fileencoding=utf-8 ts=4 sw=4 tw=79 :
 
-from __future__ import (unicode_literals, absolute_import,
-                        division, print_function)
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
